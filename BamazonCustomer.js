f47b11650fb136fb2e65af24f7dcfbf354b840ed
Guy <guypham@hotmail.com>
2016-09-21 12:44:13 -0700

    initial stages

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,42 +1,23 @@
+//require dependencies
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var table= require('cli-table')
-// require table
+var prompt = require('prompt');
+var table = require('cli-table')
+//create empty array for the products
+var products = []
+//create object for the purchase
+var purchase = {
+  id: "",
+  ProductName: "",
+  DepartmentName: "",
+  Price: "",
+  StockQuantity: ""
+}
+//create connection
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root", //Your username
     password: "guy", //Your password
     database: "bamazonDB"
-})
-
-function purchase(){
-  connection.query('SELECT * FROM Products ORDER BY ID', function(err, response){
-    for(var i=0; i<response.length; i++){
-      console.log(response[i]);
-    }
-    prompt();
-  });
-}
-//
-function prompt(){
-    inquirer.prompt({
-        name: "itemnumber",
-        type: "input",
-        message: "What is the id of the product you would like to buy?",
-    }).then(function(response){
-    inquirer.prompt([{
-      type: 'input',
-      message: 'How many would you like to buy?',
-      name: 'purchaseQuantity'
-    }]).then(function(response){
-      connection.query('SELECT StockQuantity, Price FROM Products WHERE ?', {ProductID:response.itemNum}, function(err, res){  if(res.StockQuantity -= StockQuantity) {
-            console.log("Insuffient quantity");
-          } else {
-            console.log("Order filled");
-          }
-      });
-    });
-  });
-}
-purchase();
+});
