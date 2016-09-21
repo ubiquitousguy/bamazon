2a62b50a565cbea6c2b4f4b2c9f68c3d98b77b73
Guy <guypham@hotmail.com>
2016-09-21 16:44:20 -0700

    added function for asking question and getting the product

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -3,6 +3,9 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var prompt = require('prompt');
 var table = require('cli-table')
+
+//ask question
+var productQuestion = "",
 //create empty array for the products
 var products = []
 //create object for the purchase
@@ -21,3 +24,41 @@ var connection = mysql.createConnection({
     password: "guy", //Your password
     database: "bamazonDB"
 });
+
+function purchase() {
+  connection.query('SELECT * FROM Products ORDER BY id', function(err, res){
+    //empty array
+    products = [];
+    //put result in array
+    products = res;
+
+    //ask the question
+    productQuestion = 'What is the produce you would like to buy: \n';
+
+    //add question with products
+    for(var i = 0, l = products.length; i < l; i++) {
+      productQuestion += ' (' + (products[i].item_id) + ' / ' + products[i].product_name + ' _ ' + '$' + products[i].product_price.toFixed(2) + '\n';
+    };
+
+    // initiate
+    prompt.start();
+
+    //run prompt
+    promptOrder();
+  });
+};
+//prompt Order
+  function promptOrder() {
+    var itemQty = [
+      {
+        name: 'item',
+        description: productQuestion,
+        required: true,
+        message: 'Choose item from 1' + ' to ' + products.length + '. \n',
+        conform: function(value) {
+          value = parseInt(value);
+          return value > 0 && value <= products.length
+        }
+      },
+    ]
+  }
