db8f945cb65db6b0ba4f160767f9d35f61778be6
Guy <guypham@hotmail.com>
2016-09-21 20:27:25 -0700

    prompt user to purchase function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -2,19 +2,18 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var prompt = require('prompt');
-var table = require('cli-table')
+var table = require('cli-table');
 
 //ask question
-var productQuestion = "",
+var productQuestion = "";
 //create empty array for the products
-var products = []
+var products = [];
 //create object for the purchase
 var purchase = {
   id: "",
-  ProductName: "",
-  DepartmentName: "",
-  Price: "",
-  StockQuantity: ""
+  item: "",
+  quantity: "",
+  total: ""
 }
 //create connection
 var connection = mysql.createConnection({
@@ -37,7 +36,7 @@ function purchase() {
 
     //add question with products
     for(var i = 0, l = products.length; i < l; i++) {
-      productQuestion += ' (' + (products[i].item_id) + ' / ' + products[i].product_name + ' _ ' + '$' + products[i].product_price.toFixed(2) + '\n';
+      productQuestion += ' (' + (products[i].item_id) + ' / ' + products[i].product_name + ' _ ' + '$' + products[i].price.toFixed(2) + '\n';
     };
 
     // initiate
@@ -60,5 +59,54 @@ function purchase() {
           return value > 0 && value <= products.length
         }
       },
-    ]
-  }
+      {
+        name: 'qty',
+        description: "Enter quanity",
+        required: true,
+      }
+    ];
+    prompt.get(itemQty, function(err,result){
+
+      //store result
+      purchase.id = result.item;
+      purchase.item = products[purchase.id - 1].product_name;
+      purchase.qty = result.qty;
+
+      console.log ('Your order: ' +
+     +'(' + purchase.id + ') / ' +
+      purchase.item + ', ' + purchase.qty);
+
+      //get confirmation
+      promptConfirm();
+    });
+  };
+
+  function promptConfirm() {
+    var confirmQty = [
+      {
+        name: 'confirm'
+        message: 'Confirm quanity Y/N',
+        required: true,
+        warning: 'Y or N only!'
+        validator: /^(?:|Y|n|N)$/,
+      }
+    ];
+    prompt.get(confirmQty, function(err,result) {
+      result.confirm = result.confirm.toUpperCase();
+
+      if (result.confirm == "Y") {
+        if (purchase.qty > products[purchase.id - 1].qty){
+          console.log('Insuffient quanity. \n');
+          promptOrder();
+        } else {
+          purchase.total = purchase.qty * products[purchase.id - 1].price;
+          console.log('Your total: $' + purchase.total.toFixed(2));
+          confirmOrder();
+        }
+
+      } else if (result.confirm == "N") {
+        console.log('Order cancelled. \n');
+        promptOrder();
+      }
+    });
+  };
